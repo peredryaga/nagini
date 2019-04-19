4b20e5a420d1eb3570472fb7739e253a3d4350a1
peredryaga <orlovgb@mail.ru>
2019-04-19 13:42:50 +0300

    JobManager changes * JobManager can work with many heads * Refactoring after graph and job changes * JobManager tests

--- nagini/v2/manager.py
+++ nagini/v2/manager.py
@@ -5,45 +5,48 @@ import logging
 import random
 import time
 
+from nagini.utility import flatten
 from nagini.v2.graph import ExecutionGraph
 
 
 class JobManager(object):
-    def __init__(self, head_job, logger=None):
-        self.head_job = head_job
+    def __init__(self, heads, logger=None):
+        """
+        :type heads: nagini.v2.base.JobMetaClass | list | tuple | set| dict
+        :type logger: logging.Logger
+        """
+        self.heads = flatten(heads)
         self.jobs = {}
         self.logger = logger or logging.getLogger(__name__)
 
     def build_graph(self, params):
-        graph = ExecutionGraph(self.head_job)
+        graph = ExecutionGraph(self.heads)
         graph.build(params)
         graph.validate()
         return graph
 
-    def init_job(self, job, params):
-        job = job(params=params)
-        return job
-
     def execute_job(self, job):
-        job.execute_new()
+        job.execute()
 
-    def prepare_job_input(self, node, job, params):
-        for require in (r for r in node.children if r.job_class not in self.jobs):
-            self.run_node(require, params)
+    def prepare_job_input(self, node, params):
+        for require_job in (r for r in node.children if r.job_class not in self.jobs):
+            self.run_node(require_job, params)
 
-        job.update_input(self.jobs, params=params)
+        node.job.update_input(self.jobs)
 
     def run_node(self, node, params):
         if node.job_class not in self.jobs:
-            job = self.jobs[node.job_class] = self.init_job(node.job_class, params)
+            self.jobs[node.job_class] = node.job
 
-            if not job.can_skip():
-                self.prepare_job_input(node, job, params)
-                self.execute_job(job)
+            if not node.job.can_skip():
+                self.prepare_job_input(node, params)
+                self.execute_job(node.job)
 
     def run(self, params):
         graph = self.build_graph(params)
-        self.run_node(graph.head, params)
+
+        for head in graph.heads:
+            self.run_node(head, params)
 
 
 class JobManagerWithRetries(JobManager):
---
+++ test/test_manager.py
@@ -0,0 +1,44 @@
+# -*- coding: utf8 -*-
+from __future__ import unicode_literals
+
+import pytest
+from six.moves import range
+
+from nagini.fields import IntField
+from nagini.v2.job import Job
+from nagini.v2.manager import JobManager
+
+
+class A(Job):
+    value = IntField()
+
+    def run(self):
+        return self.params.value
+
+
+class B(Job):
+    requires = A
+    value = IntField()
+
+    def configure(self):
+        self.params.value += 3
+
+    def run(self):
+        return self.input * 2 + self.params.value
+
+
+class C(Job):
+    requires = {'A': A, 'B': B}
+
+    def run(self):
+        return self.input['B'] + 2 - self.input['A']
+
+
+@pytest.mark.parametrize('value', list(range(10)))
+def test_manager(value):
+    params = {'value': value}
+
+    manager = JobManager(C)
+    manager.run(params)
+
+    assert manager.jobs[C].result == (value * 2 + value) + (2 + 3) - value
